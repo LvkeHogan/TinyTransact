01c16c1d4cbc2a7fd5eb028019e5fea643567cd9
Luke Hogan <lhogan63@gmail.com>
2024-02-13 20:53:09 -0500

    Spring Boot project created

---
+++ src/main/java/com/lukehogan/tinytransact/TinytransactApplication.java
@@ -0,0 +1,13 @@
+package com.lukehogan.tinytransact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinytransactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinytransactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukehogan/tinytransact/TinytransactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukehogan.tinytransact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinytransactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
