ac7d1079116decc69b8e273d9f4eee731afaa999
Luke Hogan <lhogan63@gmail.com>
2024-05-10 16:24:52 -0500

    Rebuilt project on newer Java and Spring.

--- src/test/java/com/lukehogan/tinytransact/TinytransactApplicationTests.java
+++ src/test/java/com/lukehogan/tinytransact/TinytransactApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class TinytransactApplicationTests {
+class TinytransactApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-	
+
 }
